dbabeee1360190617676a2ceded4be86c3c99e4f
Md Emon Hossen <mdemong87@gmail.com>
2025-08-13 13:24:41 +0600

    remove some file and folder

--- src/app/(deshboard)/admin/(others-pages)/(forms)/form-elements/page.tsx
+++ src/app/(deshboard)/admin/(others-pages)/(forms)/form-elements/page.tsx
@@ -5,7 +5,6 @@ import FileInputExample from "@/components/form/form-elements/FileInputExample";
 import InputGroup from "@/components/form/form-elements/InputGroup";
 import InputStates from "@/components/form/form-elements/InputStates";
 import RadioButtons from "@/components/form/form-elements/RadioButtons";
-import SelectInputs from "@/components/form/form-elements/SelectInputs";
 import TextAreaInput from "@/components/form/form-elements/TextAreaInput";
 import ToggleSwitch from "@/components/form/form-elements/ToggleSwitch";
 import { Metadata } from "next";
@@ -22,8 +21,6 @@ export default function FormElements() {
       <PageBreadcrumb pageTitle="From Elements"></PageBreadcrumb>
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <div className="space-y-6">
-          <DefaultInputs />
-          <SelectInputs />
           <TextAreaInput />
           <InputStates />
         </div>
