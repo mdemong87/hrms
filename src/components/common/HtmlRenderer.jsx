fbdc473df8a6af57f778c942a47f57ae52f15003
Md Emon Hossen <mdemong87@gmail.com>
2025-08-28 13:25:54 +0600

    update html render error

--- src/components/common/HtmlRenderer.jsx
+++ src/components/common/HtmlRenderer.jsx
@@ -1,9 +1,21 @@
 'use client'; // needed if you're using Next.js 13+ app directory
 
+import { useEffect, useState } from 'react';
+
 export default function HtmlRenderer({ htmlContent }) {
+
+    const [cleanHTML, setCleanHTML] = useState('');
+
+    useEffect(() => {
+        import('dompurify').then((module) => {
+            const DOMPurify = module.default;
+            setCleanHTML(DOMPurify.sanitize(htmlContent));
+        });
+    }, [htmlContent]);
+
     return (
         <div
-            dangerouslySetInnerHTML={{ __html: htmlContent }}
+            dangerouslySetInnerHTML={{ __html: cleanHTML }}
             className="prose" // optional Tailwind styling
         />
     );
