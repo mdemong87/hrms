4adea95f5351fd51e63df6e94f1c9eaace0d6c46
Md Emon Hossen <mdemong87@gmail.com>
2025-08-30 13:02:51 +0600

    leave apply function for project manager panel

---
+++ src/app/(deshboard)/projectmanager/projects/addproject/page.jsx
@@ -0,0 +1,21 @@
+'use client';
+
+import PageBreadcrumb from '@/components/common/PageBreadCrumb';
+import UnderDevelopment from '@/components/common/UnderDevelopment';
+
+const AddProject = () => {
+
+
+
+    return (
+        <div>
+
+            <PageBreadcrumb pageTitle={"Add Project"} />
+
+            <UnderDevelopment />
+
+        </div>
+    )
+}
+
+export default AddProject;
