3661159647efda97fcc27b7e01a99ea730188dcb
Md Emon Hossen <mdemong87@gmail.com>
2025-09-15 09:28:33 +0600

    simple deploye bug fixed

--- src/components/ecommerce/MonthlySalesChart.tsx
+++ src/components/ecommerce/MonthlySalesChart.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { ApexOptions } from "apexcharts";
 import dynamic from "next/dynamic";
-import { useState } from "react";
 
 // Dynamically import the ReactApexChart component
 const ReactApexChart = dynamic(() => import("react-apexcharts"), {
@@ -103,15 +102,7 @@ export default function MonthlySalesChart({ deshboardData }) {
       data: SalesData,
     },
   ];
-  const [isOpen, setIsOpen] = useState(false);
 
-  function toggleDropdown() {
-    setIsOpen(!isOpen);
-  }
-
-  function closeDropdown() {
-    setIsOpen(false);
-  }
 
   return (
     <div className="overflow-hidden rounded-2xl border border-gray-200 bg-white px-5 pt-5 dark:border-gray-800 dark:bg-white/[0.03] sm:px-6 sm:pt-6">
--- src/components/ecommerce/MonthlyTarget.tsx
+++ src/components/ecommerce/MonthlyTarget.tsx
@@ -67,7 +67,7 @@ export default function MonthlyTarget({ deshboardData }) {
               Monthly Target of
             </h3>
             <p className="mt-1 font-normal text-gray-500 text-theme-sm dark:text-gray-400">
-              Target you've set for each month
+              Target you&apos;ve set for each month
             </p>
           </div>
         </div>
